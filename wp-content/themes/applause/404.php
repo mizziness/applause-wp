a36ccd68cb170f923fead845d60aeb939c29e488
Patrick <pstonge@overdriveinteractive.com>
2023-11-09 14:54:53 -0500

    404 page

---
+++ wp-content/themes/applause/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<div id="main-content">
+	<div class="container">
+		<div id="content-area" class="clearfix">
+			<article id="post-0" <?php post_class( 'et_pb_post not_found' ); ?>>
+				<h1>Page Not Found</h1>
+
+				<p>The page you requested could not be found. Try using the navigation above to locate the page you're looking for.</p>
+
+			</article>
+
+		</div>
+	</div>
+</div>
+
+<?php
+
+get_footer();
